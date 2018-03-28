e9bca34513b7ee15a0f8e53c6419624e5cc0d501
Klis <rklis@pmiplkral38294.pmintl.net>
2018-03-28 13:59:46 +0200

    New things

--- test/test_iter_fibb.py
+++ test/test_iter_fibb.py
@@ -6,4 +6,10 @@ fibonacci_iter = Fibonacci.fibonacci_iter
 
 class TestFibonacciIter(TestCase):
 
-   pass
+   def test_first_iter(self):
+      input = [0, 1, 2, 3, 4, 5, 6, 7]
+      expected_result = [0, 1, 1, 2, 3, 5, 8, 13]
+
+      for single_input, single_expected in zip(input, expected_result):
+         self.assertEqual(Fibonacci.fibonacci_iter(single_input), single_expected)
+
--- test/test_mat_function.py
+++ test/test_mat_function.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from math import sqrt
 
+
 from math_functions.mat_functions import private_pow, Fibonacci
 
 
@@ -10,10 +11,18 @@ class MatFunctionsTests(TestCase):
 
 
 class FibonacciTests(TestCase):
+    def test_first_tc(self):
+        input = [0,1,2,3,4,5,6,7]
+        expected_result = [0,1,1,2,3,5,8,13]
 
-    pass
+        for single_input, single_expected in zip(input, expected_result):
+            self.assertEqual(Fibonacci.fibonacci(single_input),single_expected)
 
 
+    def test_zero(self):
+        input = 0
+        expected = 0
+        self.assertEqual(Fibonacci.fibonacci(input),expected)
 
 
 
